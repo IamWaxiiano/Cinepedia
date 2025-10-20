0796e8dec407e6138b31e51bd1cc5ee1b032faf7
Maxi <maximiliano.gutierrez.g@institutocomercialosorno.com>
2025-10-20 11:16:59 -0300

    commit 2 modelo usuario terminado y pelicula avanzado

--- flask_app/models/usuario.py
+++ flask_app/models/usuario.py
@@ -1,4 +1,5 @@
 from flask_app.config.conexion import Conexion
+import bcrypt
 
 class Usuario:
     def __init__(self, data):     
@@ -7,10 +8,38 @@ class Usuario:
         self.apellido = data['apellido']
         self.email = data['email']
         self.password = data['password']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO usuarios (nombre, apellido, email, password) VALUES (%(nombre)s, %(apellido)s, %(email)s, %(password)s);"
-        return Conexion('cinepedia').query_db(query, data)
+        hashed_password=bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
+        data["password"]=hashed_password
+        usuario= cls.ValidarEmail(data["email"])
+        if usuario is None:
+            query = "INSERT INTO usuarios (nombre, apellido, email, password) VALUES (%(nombre)s, %(apellido)s, %(email)s, %(password)s);"
+            return Conexion('cinepedia').query_db(query, data)
+        else: return None
         
+    @classmethod
+    def ValidarEmail(cls, email):
+        query= "SELECT * from usuarios where email=%(email)s"
+        data = {"email": email}
+        usuario=Conexion('cinepedia').query_db(query, data)
+        return usuario[0] if usuario else None
+    
+    @classmethod
+    def get_all(cls):
+        query= "SELECT * from usuarios"
+        usuarios_en_bd= Conexion('cinepedia').query_db(query)
+        usuarios=[]
+        for usuario in usuarios_en_bd:
+            usuarios.append(cls(usuario))
+        return usuarios
+    
+    @classmethod
+    def login(cls,data):
+        query= "SELECT * from usuarios where email=%(email)s"
+        data = {"email": data["email"]}
+        usuario= Conexion('cinepedia').query_db(query, data)
+        hashed_password=usuario[0]["password"].encode('utf-8')
+        if bcrypt.check_password_hash(data["password"].encode('utf-8'),hashed_password):
+            return usuario[0] if usuario else None
